8257a7fc5e4c147db6edfe245c81b8073d3124ff
siilinreba <63076655+siilinreba@users.noreply.github.com>
2020-06-14 17:36:19 +0300

    WP2Static

--- wp-content/themes/twentytwenty/assets/js/editor-script-block.js
+++ wp-content/themes/twentytwenty/assets/js/editor-script-block.js
@@ -1,1 +1,9 @@
-wp.domReady(function(){wp.blocks.unregisterBlockStyle('core/button','squared');});
+/**
+ * Remove squared button style
+ *
+ * @since Twenty Twenty 1.0
+ */
+/* global wp */
+wp.domReady( function() {
+	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
+} );
